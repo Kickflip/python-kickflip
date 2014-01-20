64429aabfa4e3666c5e29d707da7970757d9c52c
Rich Jones <rich@anomos.info>
2014-01-19 23:15:49 -0800

    Command line client

--- kickflip/kickflip.py
+++ kickflip/kickflip.py
@@ -1,14 +1,21 @@
+import envoy
 import boto
+import requests
+from requests_oauthlib import OAuth2Session
 from boto.s3.connection import Location
 from boto.s3.lifecycle import Lifecycle, Transition, Rule
 
 connected = False
+connected_aws = False
 
 KICKFLIP_API_KEY = ''
-KICKFLIP_SECRET_API_KEY = '' 
+KICKFLIP_SECRET_API_KEY = ''
 
-KICKFLIP_ACCESS_TOKEN = ''
-KICKFLIP_SECRET_ACCESS_TOKEN = '' 
+KICKFLIP_CLIENT_ID = ''
+KICKFLIP_CLIENT_SECRET = ''
+
+KICKFLIP_BASE_URL = 'https://funkcity.ngrok.com/'
+KICKFLIP_API_URL = KICKFLIP_BASE_URL + '/api/'
 
 AWS_ACCESS_KEY = ''
 AWS_SECRET_ACCESS_KEY = ''
@@ -30,16 +37,16 @@ def set_aws_keys(AWS_ACCESS_KEY_VAR, AWS_SECRET_ACCESS_KEY_VAR):
 
 def connect_aws():
 
-    global connected
+    global connected_aws
     global AWS_ACCESS_KEY
     global AWS_SECRET_ACCESS_KEY
     global s3
 
-    if not connected:
+    if not connected_aws:
         s3 = boto.connect_s3(AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY)
-        connected = True
+        connected_aws = True
 
-    return connected
+    return connected_aws
 
 def upload_file(filename):
     return True
@@ -48,16 +55,24 @@ def upload_file(filename):
 ### Kickflip Auth
 ###################
 
-def set_keys(key, secret_key):
-    global KICKFLIP_API_KEY
-    global KICKFLIP_SECRET_API_KEY
+def connect():
+    keys = load_keys()
+    set_keys(key, secret_key)
+    get_access_tokens()
 
-    KICKFLIP_API_KEY = key
-    KICKFLIP_SECRET_API_KEY = secret_key
+def set_keys(client_id, client_secret):
+    global KICKFLIP_CLIENT_ID
+    global KICKFLIP_CLIENT_SECRET
+
+    KICKFLIP_CLIENT_ID = client_id
+    KICKFLIP_CLIENT_SECRET = client_secret
 
 def get_access_tokens():
     global KICKFLIP_ACCESS_TOKEN
     global KICKFLIP_SECRET_ACCESS_TOKEN
+
+    kickflip_session = OAuth2Session(KICKFLIP_ACCESS_TOKEN, token=KICKFLIP_SECRET_ACCESS_TOKEN)
+    kickflip_session.authorization_url(KICKFLIP_BASE_URL + 'o/authorize/')
 
     # requests-oauth.get_tokens()
     KICKFLIP_ACCESS_TOKEN = key
@@ -78,7 +93,9 @@ def create_user(username):
 def get_user(username):
     return ''
 
-def start_stream(stream_name):
+def start_stream(file_path, stream_name=None):
+
+    stream_video(file_path)
     return ''
 
 def pause_stream(stream_name):
@@ -93,12 +110,19 @@ def stop_stream():
 
 def stream_video(video_path):
 
-    args =  '-v 9 -loglevel 99 -re -i %s -an ' + 
-            '-c:v libx264 -b:v 128k -vpre ipod320 \ ' +
-            ' -flags -global_header -map 0 -f segment -segment_time 4 ' +
+    args =  '-v 9 -loglevel 99 -re -i %s -an ' + \
+            '-c:v libx264 -b:v 128k -vpre ipod320 \ ' + \
+            ' -flags -global_header -map 0 -f segment -segment_time 4 ' + \
             ' -segment_list test.m3u8 -segment_format mpegts stream.ts'
     args = args % video_path
     process = envoy.run ('ffmpeg ' + args)
 
     upload_file(video_path)
     return ''
+
+####################
+### AWS
+####################
+
+def upload_files(streamable_files_directory):
+    return True
---
+++ kickflip/kickflip_cli.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python
+
+from clint.textui import colored, puts, progress
+from clint import resources
+import sys
+import argparse
+import getpass
+import simplejson as json
+
+import kickflip
+
+def main():
+    parser = argparse.ArgumentParser(description='Kickflip. Stream video directly to Kickflip.io.\n')
+    parser.add_argument('file_path', metavar='U', type=str,
+                   help='Path to the file to stream')
+
+    args = parser.parse_args()
+    vargs = vars(args)
+    if not any(vargs.values()):
+        parser.error('Please supply a file to stream!')
+
+    read_keys_cli = False
+    client_id, client_secret = load_keys(read_keys_cli)
+    user_access_key, user_access_secret_key = load_user(getpass.getuser())
+
+    file_path = vargs['file_path']
+    puts(colored.green('Streaming') + ': ' + file_path)
+
+    #kickflip.set_keys(client_id, client_secret)
+    #kickflip.set_user(resources.user)
+    kickflip.start_stream(file_path)
+
+def load_keys(read_cli=False):
+
+    resources.init('Kickflip', 'Kickflip')
+    config_json = resources.user.read('config.json')
+
+    settings = json.loads(config_json)
+    if not settings.has_key('client_id') or read_cli:
+        settings['client_id'] = raw_input("What is your client ID? ")
+        resources.user.write('config.json', json.dumps(settings, sort_keys=True))
+    if not settings.has_key('client_secret') or read_cli:
+        settings['client_secret'] = raw_input("What is your client secret? ")
+        resources.user.write('config.json', json.dumps(settings, sort_keys=True))
+
+    return settings['client_id'], settings['client_secret']
+
+def load_user(username):
+    resources.init('Kickflip', 'Kickflip')
+    config_json = resources.user.read('config.json')
+
+    settings = json.loads(config_json)
+    if not settings.has_key('username'):
+        settings['username'] = username
+        resources.user.write('config.json', json.dumps(settings, sort_keys=True))
+    if not settings.has_key('user_access_key'):
+        user = kickflip.create_user(username)
+        settings['user_access_key'] = 1234
+        settings['user_secret_access_key'] = 1234
+        resources.user.write('config.json', json.dumps(settings, sort_keys=True))
+
+    return settings['user_access_key'], settings['user_secret_access_key']
+
+if __name__ == '__main__':
+    try:
+        sys.exit(main())
+    except Exception, e:
+        print e
